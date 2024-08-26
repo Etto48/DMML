64c78dc14bc7d55a7d81544b4c9a662829d6d913
Ettore Ricci <minecraft.etto@gmail.com>
2024-08-26 02:28:18 +0200

    Added color name demo

--- dmml/utils/colors.py
+++ dmml/utils/colors.py
@@ -26,12 +26,19 @@ def avg_colors(color_1: str, color_2: str) -> str:
 
 def rgb_to_name(r: float, g: float, b: float) -> str:
     colors = [
-        ("red", (1, 0, 0)),
-        ("green", (0, 1, 0)),
-        ("blue", (0, 0, 1)),
-        ("yellow", (1, 1, 0)),
-        ("cyan", (0, 1, 1)),
-        ("magenta", (1, 0, 1)),
+        ("red", 0/360),
+        ("orange", 30/360),
+        ("yellow", 60/360),
+        ("lime", 90/360),
+        ("green", 120/360),
+        ("teal", 150/360),
+        ("cyan", 180/360),
+        ("azure", 210/360),
+        ("blue", 240/360),
+        ("violet", 270/360),
+        ("magenta", 300/360),
+        ("rose", 330/360),
+        ("red", 360/360),
     ]
     def distance_hue(h1: float, h2: float) -> float:
         d = abs(h1 - h2)
@@ -39,13 +46,25 @@ def rgb_to_name(r: float, g: float, b: float) -> str:
     closest = None
     distance_to_closest = float("inf")
     hsv = colorsys.rgb_to_hsv(r, g, b)
-    for name, color in colors:
-        hsv_color = colorsys.rgb_to_hsv(*color)
-        d = distance_hue(hsv[0], hsv_color[0])
+    for name, hue in colors:
+        d = distance_hue(hsv[0], hue)
         if d < distance_to_closest:
             closest = name
             distance_to_closest = d
-    return closest
+    name = closest
+    if hsv[1] < 0.1:
+        if hsv[2] < 0.1:
+            name += "ish black"
+        elif hsv[2] > 0.9:
+            name += "ish white"
+        else:
+            name += "ish gray"
+    else:
+        if hsv[2] < 0.6:
+            name = "dark " + name
+        elif hsv[2] > 0.95:
+            name = "light " + name
+    return name
 
 def random_color() -> str:
     
@@ -72,14 +91,22 @@ if __name__ == "__main__":
         colors[i,2] = color[2]
         avg_color += color
     avg_color /= RANDOM_COLORS_N
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 3, 1)
     plt.imshow(colors.reshape(sqrt_n, sqrt_n, 3))
     plt.title(f"{RANDOM_COLORS_N} random colors")
     plt.axis("off")
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 2)
     plt.imshow(np.array([[avg_color]]))
     plt.title(f"Average color {rgb_to_str(*avg_color)}")
     plt.axis("off")
+    
+    plt.subplot(1, 3, 3)
+    plt.imshow(colors.reshape(sqrt_n, sqrt_n, 3))
+    for i, color in enumerate(RANDOM_COLORS):
+        plt.text(i % sqrt_n, i // sqrt_n, rgb_to_name(*str_to_rgb(color)), color="white", fontsize=8, ha="center", va="center")
+    plt.title("Names (zoom in)")
+    plt.axis("off")
+    
     plt.show()
     
     
