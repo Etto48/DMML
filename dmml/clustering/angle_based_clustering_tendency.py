918cc2bb0d897660a82718df7512fa2536c3562f
Ettore Ricci <minecraft.etto@gmail.com>
2024-08-29 03:10:32 +0200

    Better (correct) angle based outlier detection and clustering tendency

---
+++ dmml/clustering/angle_based_clustering_tendency.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.append(f'{__file__}/../../../')
+from dmml.outlier_detection.angle_based import var_cosine_similarity
+import numpy as np
+
+def angle_based_clustering_tendency(dataset: list[list[float]], num_samples: int) -> float:
+    N = len(dataset)
+    N_dim = len(dataset[0])
+    
+    ranges = [[dataset[0][i], dataset[0][i]] for i in range(N_dim)]
+    for i in range(N):
+        for j in range(N_dim):
+            if ranges[j][0] > dataset[i][j]:
+                ranges[j][0] = dataset[i][j]
+            if ranges[j][1] < dataset[i][j]:
+                ranges[j][1] = dataset[i][j]
+    
+    p = [np.random.randint(0, N) for _ in range(N)]
+    q = [[np.random.uniform(ranges[j][0], ranges[j][1]) for j in range(N_dim)] for _ in range(N)]
+    
+    random_measures = num_samples
+    
+    p_similarity = [var_cosine_similarity(np.array(dataset), i, random_measures) for i in p]
+    q_similarity = [var_cosine_similarity(np.array(dataset), np.array(i), random_measures) for i in q]
+    
+    a = sum(p_similarity) / (sum(p_similarity) + sum(q_similarity))
+    
+    return a
+
+if __name__ == "__main__":
+    from dmml.clustering.datasets import random_dataset, demo_dataset
+    from dmml.clustering.plotting import plot_dataset
+    import matplotlib.pyplot as plt
+    
+    #dataset = random_dataset(500, 0.05, [0, 0], [10, 10])
+    dataset = demo_dataset(500, "three_lines")
+    uniform = demo_dataset(500, "uniform")
+    h_d = angle_based_clustering_tendency(dataset, 100)
+    h_u = angle_based_clustering_tendency(uniform, 100)
+    plt.subplot(1, 2, 1)
+    plot_dataset(dataset)
+    plt.title(f"ABCT test: {h_d}")
+    plt.subplot(1, 2, 2)
+    plot_dataset(uniform)
+    plt.title(f"ABCT test: {h_u}")
+    plt.show()
+    
--- dmml/outlier_detection/angle_based.py
+++ dmml/outlier_detection/angle_based.py
@@ -1,30 +1,46 @@
 import numpy as np
 
-def cosine_similarity(x, a, b, normalized: bool = True) -> float:
+def cosine_similarity(x, a, b) -> float:
     v1 = x - a
     v2 = x - b
-    distance = np.linalg.norm(v1) * np.linalg.norm(v2) + 1e-10
-    if normalized:
-        return np.dot(v1, v2) / distance
-    else:
-        return np.dot(v1, v2), distance
+    distance = np.dot(v1, v1) * np.dot(v2, v2) + 1e-10
+    return np.dot(v1, v2) / distance
 
-def angle_based_outlier_detection(dataset: np.ndarray, threshold: float, iterations: int, distance_weighted: bool = False) -> list[int]:
+def random_index_except(len_dataset: int, indices: list[int]) -> list[float]:
+    indices = sorted(list(set(indices)))
+    assert len(indices) < len_dataset
+    assert all(0 <= i < len_dataset for i in indices)
+    i = np.random.randint(0, len_dataset - len(indices))
+    for j in indices:
+        if i >= j:
+            i += 1
+    return i
+
+def var_cosine_similarity(dataset: np.ndarray, x: int | np.ndarray, iterations: int) -> float:
+    if isinstance(x, int):
+        i = x
+        x = dataset[x]
+    else:
+        i = None
+    values = []
+    for _ in range(iterations):
+        if i is None:
+            a_index = random_index_except(len(dataset), [])
+            b_index = random_index_except(len(dataset), [a_index])
+        else:
+            a_index = random_index_except(len(dataset), [i])
+            b_index = random_index_except(len(dataset), [i, a_index])
+        a = dataset[a_index]
+        b = dataset[b_index]
+        similarity = cosine_similarity(x, a, b)
+        values.append(similarity)
+    return np.var(values)
+
+def angle_based_outlier_detection(dataset: np.ndarray, threshold: float, iterations: int) -> list[int]:
     outliers = []
-    for i, x in enumerate(dataset):
-        avg_similarity = 0
-        total_distance = 0
-        for _ in range(iterations):
-            a, b = dataset[np.random.choice(len(dataset), 2, replace=False)]
-            if distance_weighted:
-                similarity, distance = cosine_similarity(x, a, b, normalized=False)
-                total_distance += distance
-                avg_similarity += similarity
-            else:
-                total_distance += 1
-                avg_similarity += cosine_similarity(x, a, b, normalized=True)
-        avg_similarity /= total_distance
-        if avg_similarity > threshold:
+    for i in range(len(dataset)):
+        std_similarity = var_cosine_similarity(dataset, i, iterations)
+        if std_similarity < threshold:
             outliers.append(i)
     return outliers
 
@@ -36,5 +52,5 @@ if __name__ == "__main__":
     print(f"Injecting outlier at index {outlier_index}")
     dataset[outlier_index] += 3
     
-    outliers = angle_based_outlier_detection(dataset, 0.9, 10, True)
+    outliers = angle_based_outlier_detection(dataset, 0.000000005, 10)
     print(f"Detected outliers: {outliers}")
